5feebb5dc39bc84ffd6bd8461a13bc7c3692b738
Spidey6978 <91067955+Spidey6978@users.noreply.github.com>
2026-01-21 17:02:41 +0530

    Add @HiveType annotations to Contact model for serialization

--- lib/home_screen/contact.dart
+++ lib/home_screen/contact.dart
@@ -1,6 +1,16 @@
+import 'package:hive/hive.dart';
+
+part 'contact.g.dart';
+
+@HiveType(typeId: 0)
 class Contact {
+  @HiveField(0)
   final String name;
+
+  @HiveField(1)
   final double amount;
+
+  @HiveField(2)
   final String type;
 
   Contact({required this.name, required this.amount, required this.type});
