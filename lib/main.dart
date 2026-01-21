6c39eb29e573f00df19c6e0ae600b642eba56a66
Spidey6978 <91067955+Spidey6978@users.noreply.github.com>
2026-01-21 17:05:36 +0530

    Initialize Hive and ContactAdapter in main.dart
    Updated main function to initialize Hive and changed MyApp from StatelessWidget to StatefulWidget for better state management.

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'home_screen/home_page.dart'; 
+import 'package:hive_flutter/hive_flutter.dart';
+import 'home_screen/home_page.dart';
 import 'home_screen/contacts_provider.dart';
+import 'home_screen/contact.dart';
 
-void main() {
+void main() async {
+  // Initialize Hive
+  await Hive.initFlutter();
+  Hive.registerAdapter(ContactAdapter());
+
   runApp(
     ChangeNotifierProvider(
-      create: (context) =>  ContactProvider(),
+      create: (context) => ContactProvider(),
       child: const MyApp(),
     ),
   );
 }
 
-class MyAppState extends ChangeNotifier {
+class MyAppState extends ChangeNotifier {}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    // Initialize Hive after provider is created
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      context.read<ContactProvider>().initHive();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      title: 'Expense Tracker',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        useMaterial3: true,
+        brightness: Brightness.dark,
       ),
       home: const HomePage(),
     );
